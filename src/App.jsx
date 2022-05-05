f49e0fb5e65704748574660294acf7ff105c9eb8
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-05 14:55:57 +0200

    npm sass installed and style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import "./App.scss";
 
-function App() {
-  return <div className="App">Hello!</div>;
-}
+const howtosUrl = "https://edwardtanguay.netlify.app/share/howtos.json";
 
-export default App;
+export const App = () => {
+  const [howtos, setHowtos] = useState([]);
+
+  useEffect(() => {
+    const getReactHowtos = (_howtos, category = "react") => {
+      return _howtos.filter(
+        (m) => m.category.toLowerCase() === category.toLowerCase()
+      );
+    };
+
+    (async () => {
+      const response = await fetch(howtosUrl);
+      let _howtos = await response.json();
+      _howtos = getReactHowtos(_howtos, "react");
+      setHowtos(_howtos);
+    })();
+  }, []);
+
+  return (
+    <div className="App">
+      <h2>Fetch, filter and display data from an API</h2>
+      <div>There are {howtos.length} React howtos.</div>
+      <ul>
+        {howtos.map((howto, index) => {
+          return (
+            <li key={index}>
+              <a
+                target="_blank"
+                href={`https://edwardtanguay.netlify.app/howtos?id=${howto.id}`}
+                rel="noreferrer"
+              >
+                {howto.category}: {howto.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
